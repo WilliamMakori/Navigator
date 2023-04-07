df79539d552333df0be35d2c2dd2c04e8e969ab3
Wchacha <wchacha@sfu.ca>
2023-04-07 01:31:13 -0700

    Three buttons.

--- Navigator/Views/BottomSheetView.swift
+++ Navigator/Views/BottomSheetView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 import MapKit
 
-struct Home:View{
+struct Home:View {
     
-    
+
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 13.086, longitude: 80.27), latitudinalMeters: 10000, longitudinalMeters: 10000)
     @State var offset: CGFloat = 0
     
     var body: some View {
         
         ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
+            
             Map(coordinateRegion: $region)
                 .ignoresSafeArea(.all,edges: .all)
+            
             GeometryReader { reader in
+                
                 VStack{
                     BottomSheet(offset: $offset, value: (-reader.frame(in: .global).height + 150 ))
                         .offset(y: reader.frame(in: .global).height - 150)
@@ -37,15 +40,12 @@ struct Home:View{
                                     if value.translation.height < 0 && offset > (-reader.frame(in: .global).height + 150){
                                         offset = value.translation.height
                                     }
-                                    
-                                    
                                 }
                                 // Read up on gestures again
                                 if value.startLocation.y > reader.frame(in: .global).minX{
                                     
                                     if value.translation.height > 0 && offset < 0{
                                         offset = (-reader.frame(in: .global).height +  150) + value.translation.height
-                                        
                                     }
                                     
                                 }
@@ -81,7 +81,6 @@ struct Home:View{
                             }
                         })
                         )
-                    
                 }
                 
             }
@@ -112,10 +111,49 @@ struct Home:View{
                             //Three buttons at the bottom that create different pop up views.
                             // Four buttons
                             //Share My Location - impements search feature
+                            
+                            Button("Share my location") {
+                                // The code here should bring out the share pop up menu.
+                                // I think this is built in, shouldn't be too difficult
+                            }
+                           .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height*0.04)
+                            .buttonBorderShape(.roundedRectangle)
+                            .buttonStyle(.bordered)
+                            .controlSize(.large)
+                            
                             // Mark my location
+                            Button("Share my location") {
+                                // The code here should bring out the share pop up menu.
+                                // I think this is built in, shouldn't be too difficult
+                            }
+                           .frame(width: UIScreen.main.bounds.width)
+                            .buttonBorderShape(.roundedRectangle)
+                            .buttonStyle(.bordered)
+                            .controlSize(.large)
+                            .padding(.top)
+                            
                             // Report an issue
+                            
+                            Button("Share my location"){
+                                // The code here should bring out the share pop up menu.
+                                // I think this is built in, shouldn't be too difficult
+                            }
+                            
+                           .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height*0.04)
+                            .buttonBorderShape(.roundedRectangle)
+                            .buttonStyle(.bordered)
+                            .controlSize(.large)
+                            .padding(.top)
+                            
                             // Terms and conditions
                             
+                            Button {
+                                // opens link in safari that shows the terms and conditions of the app
+                            } label: {
+                                Text("Terms and conditions >")
+                                    .underline()
+                            }
+
                         })
                         .padding()
                         .padding(.top)
@@ -128,7 +166,7 @@ struct Home:View{
             }
             
 //            .background(BlurView(style: .systemMaterial))
-//            .cornerRadius(15)
+            .cornerRadius(15)
             .onChange(of: query) { newValue in
                 // observeablObject.searchResults = observeableObject.searchResults.filter ({ location in
                 // if location.house_number.contains(query)
@@ -147,5 +185,12 @@ struct Home:View{
         static var previews: some View {
             Home()
         }
+    }
+}
+struct BottomSheetMenu: View{
+    var body: some View{
+        Text("All the views in the menu below")
+        Text("recents")
+        Text("")
     }
 }
